38509b4f34a4ba86792c445e95afde62fb332c25
Patryk <urbanek32@o2.pl>
2015-01-13 12:55:43 +0100

    License update

--- lolkey.c
+++ lolkey.c
@@ -1,6 +1,20 @@
 /*
- * This is the template file used to build a system
- * specific kernel module.
+ *   https://github.com/urbanek32/lolkey
+ *   Simple keylogger made as Linux Kernel Module.
+ *   Copyright (C) 2015  Patryk Urban (urbanek32<at>github.com)
+ *
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 #include <linux/init.h>
@@ -19,7 +33,7 @@
 #include <linux/semaphore.h>
 #include <linux/wait.h>
 
-MODULE_LICENSE("Apache");
+MODULE_LICENSE("GPL");
 
 /*
  * -- Global defines --
